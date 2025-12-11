faa5c8a54bab5807965f74148b4f9937465961c9
Soriani33 <riani.soufiane@gmail.com>
2025-12-11 07:20:33 +0100

    refactor: Remove unused process.env definition
    The `process.env` definition in vite.config.ts was unnecessary and can be removed.

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // Cela permet d'éviter les crashs si une librairie utilise process.env
-    'process.env': {} 
-  }
 });
